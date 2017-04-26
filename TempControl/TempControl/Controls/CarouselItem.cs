8adc7a293a59c7e32fe9c0060e84eaa409702c8d
h82258652 <h82258652@hotmail.com>
2017-04-26 17:03:37 +0800

    update

--- TempControl/TempControl/Controls/CarouselItem.cs
+++ TempControl/TempControl/Controls/CarouselItem.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Documents;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+﻿using Windows.UI.Xaml.Controls.Primitives;
 
 namespace TempControl.Controls
 {
@@ -16,7 +6,7 @@ namespace TempControl.Controls
     {
         public CarouselItem()
         {
-            this.DefaultStyleKey = typeof(CarouselItem);
+            DefaultStyleKey = typeof(CarouselItem);
         }
     }
 }
