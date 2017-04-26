ba8c94c5c033c48e8b9c15ddcc98e2cd834de0ea
h82258652 <h82258652@hotmail.com>
2017-04-26 17:01:50 +0800

    update

---
+++ TempControl/TempControl/Controls/Carousel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TempControl.Controls
+{
+    public class Carousel : FlipView
+    {
+        public static readonly DependencyProperty IsLoopEnabledProperty = DependencyProperty.Register(nameof(IsLoopEnabled), typeof(bool), typeof(Carousel), new PropertyMetadata(false, OnIsLoopEnabledChanged));
+
+        public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register(nameof(Orientation), typeof(Orientation), typeof(Carousel), new PropertyMetadata(Orientation.Horizontal));
+
+        public Carousel()
+        {
+            DefaultStyleKey = typeof(Carousel);
+        }
+
+        public bool IsLoopEnabled
+        {
+            get
+            {
+                return (bool)GetValue(IsLoopEnabledProperty);
+            }
+            set
+            {
+                SetValue(IsLoopEnabledProperty, value);
+            }
+        }
+
+        public Orientation Orientation
+        {
+            get
+            {
+                return (Orientation)GetValue(OrientationProperty);
+            }
+            set
+            {
+                SetValue(OrientationProperty, value);
+            }
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+
+        private static void OnIsLoopEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TempControl/TempControl/Controls/CarouselItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+namespace TempControl.Controls
+{
+    public class CarouselItem : SelectorItem
+    {
+        public CarouselItem()
+        {
+            this.DefaultStyleKey = typeof(CarouselItem);
+        }
+    }
+}
